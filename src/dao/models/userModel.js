90c1262f993b9001e5a948723d67713665eb7e17
Victor Molina <molinavitillo@gmail.com>
2024-05-22 12:29:43 -0300

    features

--- src/dao/models/userModel.js
+++ src/dao/models/userModel.js
@@ -2,15 +2,19 @@ import mongoose from "mongoose";
 const usersCollection = "users";
 const usersSchema = new mongoose.Schema(
   {
-    name: String,
+    first_name: String,
+    last_name: String,
     email: {
       type: String,
       unique: true,
     },
+    age: Number,
     password: String,
-    rol: String,
+    cart: { type: mongoose.Types.ObjectId, ref: "carts" },
+    role: { type: String, default: "user" },
   },
   {
+    timestamps: true,
     strict: false,
   }
 );
